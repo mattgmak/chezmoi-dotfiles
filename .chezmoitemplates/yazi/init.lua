8af59a1a0a2b023fe9d21a4a710a329f0063d9c5
mgm3592095 <u3592095@connect.hku.hk>
2024-12-10 11:30:15 +0800

    Update config

--- .chezmoitemplates/yazi/init.lua
+++ .chezmoitemplates/yazi/init.lua
@@ -1,11 +1,11 @@
 require("starship"):setup()
 
 require("yaziline"):setup({
-    separator_style = "angly", -- "angly" | "curvy" | "liney" | "empty"
-    separator_open = "",
-    separator_close = "",
-    separator_open_thin = "",
-    separator_close_thin = "",
+    separator_style = "curvy", -- "angly" | "curvy" | "liney" | "empty"
+    -- separator_open = "",
+    -- separator_close = "",
+    -- separator_open_thin = "",
+    -- separator_close_thin = "",
     select_symbol = "",
     yank_symbol = "󰆐",
     filename_max_length = 24, -- trim when filename > 24
